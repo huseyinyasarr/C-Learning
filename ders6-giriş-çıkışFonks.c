a115e964cc1c181adedefee9bea01268769300e4
huseyinyasarr <huseyin_yasar20@erdogan.edu.tr>
2021-12-09 01:54:38 +0300

    lession6

---
+++ ders6-giriş-çıkışFonks.c
@@ -0,0 +1,28 @@
+ #include<stdio.h>
+ #include <locale.h>
+ 
+ 
+ /*
+ scanf = giriþ fonksiyonu kullanýcýdan veri almamýzý saðlar.
+ printf = çýkýþ fonksiyonu kullanýcýya çýktý verir.
+ 
+ scanf ile kullanýcýdan aldýðýmýz veriyi ekrana yazdýrmak için & kullanýlýr. ÖR yazdýrma: printf("%d",&x)
+ 
+ 
+ 
+ */
+ 
+ int main() {
+ 	
+	setlocale(LC_ALL, "Turkish");
+	
+	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	return 0;
+ }
